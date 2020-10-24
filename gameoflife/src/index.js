6e0e8222d8901d6d02fb03fec2bb0edbe896961a
Kevin Silvestre <kevsilvo@gmail.com>
2020-10-24 19:39:11 -0400

    creating the grid

--- gameoflife/src/index.js
+++ gameoflife/src/index.js
@@ -2,15 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Main extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      generation = 0,
+    }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  }
+  render() {
+    return (
+      <div>
+        <h1>Game Of Life</h1>
+        <Grid />
+        <h2>Generations: {this.state.generation}</h2>
+
+      </div >
+    );
+
+  }
+}
+
+
+ReactDOM.render(<Main />,
+  document.getElementById("root")
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
